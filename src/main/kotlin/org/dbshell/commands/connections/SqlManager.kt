9b5abae0f79186a7723cd375f8683876aea88377
Brad Miller <bfm@bradfordmiller.org>
2021-11-16 17:23:12 -0500

    Started refactoring commands

--- src/main/kotlin/org/dbshell/commands/connections/SqlManager.kt
+++ src/main/kotlin/org/dbshell/commands/connections/SqlManager.kt
@@ -6,22 +6,30 @@ import org.dbshell.actions.sql.RunSqlCommands
 
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
+import org.springframework.shell.standard.ShellOption
 import java.io.File
 
 @ShellComponent
 class SqlManager: UIManager {
     @ShellMethod("Run a SQL query")
-    fun runQuery(sql: String, rowLimit: Long =  50, executeAsync: Boolean = false) {
+    fun runQuery(sql: String, rowLimit: Long =  50, @ShellOption(defaultValue = "false") executeAsync: Boolean) {
         val rq = RunQuery(sql, rowLimit)
         executeAction(rq, executeAsync)
     }
     @ShellMethod("Export SQL query to csv")
-    fun exportQueryToCsv(sql: String, outputFile: File, separator: String = ",", includeHeaders: Boolean = true, fileExtension: String = ".csv", executeAsync: Boolean = false)  {
+    fun exportQueryToCsv(
+        sql: String,
+        outputFile: File,
+        @ShellOption(defaultValue = ",") separator: String,
+        @ShellOption(defaultValue = "true") includeHeaders: Boolean,
+        @ShellOption(defaultValue = ".csv") fileExtension: String,
+        @ShellOption(defaultValue = "false") executeAsync: Boolean
+    )  {
         val exportCsv = ExportQueryToCsv(sql, outputFile, separator, includeHeaders, fileExtension)
         executeAction(exportCsv, executeAsync)
     }
     @ShellMethod("Run SQL commands")
-    fun runSqlCommands(sql: String, executeAsync: Boolean) {
+    fun runSqlCommands(sql: String, @ShellOption(defaultValue = "false") executeAsync: Boolean) {
         val runSqlCommands = RunSqlCommands(sql)
         executeAction(runSqlCommands, executeAsync)
     }
