efa92152c761f059b69531e7266974e7fa93b4f4
Brad Miller <bfm@bradfordmiller.org>
2021-11-22 15:39:12 -0500

    Fixed NPE when incorrect job id submitted to get job results

--- src/main/kotlin/org/dbshell/shellmethods/JobMethods.kt
+++ src/main/kotlin/org/dbshell/shellmethods/JobMethods.kt
@@ -1,6 +1,5 @@
 package org.dbshell.shellmethods
 
-import org.dbshell.actions.ActionExecutor
 import org.dbshell.actions.ActionRenderer
 import org.dbshell.providers.JobValueProvider
 import org.dbshell.results.ResultsHashMap
@@ -17,7 +16,13 @@ class JobMethods: ActionRenderer {
     }
     @ShellMethod("Get job results")
     fun getJobResults(@ShellOption(valueProvider = JobValueProvider::class)uuid: UUID) {
-        val result = ResultsHashMap.resultsMap[uuid]!!
-        renderAction(result)
+        val result = ResultsHashMap.resultsMap[uuid]
+        if(result == null) {
+            println(
+                "Job id $uuid not found in job queue. Either the id is incorrect or has been deleted from the queue."
+            )
+        } else {
+            renderAction(result)
+        }
     }
 }
